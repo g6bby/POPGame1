06270ddc0e5ba93c3db1553d9ba3f968c8e5c389
g6bby <nabag123@newschool.edu>
2023-09-01 19:42:55 -0400

    Entire bubble animation done

--- Assets/Scripts/BubbleAnim.cs
+++ Assets/Scripts/BubbleAnim.cs
@@ -7,15 +7,27 @@ public class BubbleAnim : MonoBehaviour
     public Animator animator;
     public GameObject player;
     public GameObject bubble;
+    public AudioSource bubbleAudio;
 
     private void Start()
     {
         animator.gameObject.GetComponent<Animator>().enabled = false;
+
     }
 
-   private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
+        bubbleAudio.Play();
         animator.gameObject.GetComponent<Animator>().enabled = true;
+
+        animDelay();
+
+
+    }
+
+   public IEnumerator animDelay()
+    {
+        yield return new WaitForSeconds(0.001f);
         gameObject.SetActive(false);
 
     }
